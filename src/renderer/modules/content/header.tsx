6c0aa56ade55bf7be3d0fbc4686849fa23c7aa38
TheBoringDude <iamcoderx@gmail.com>
2022-01-17 21:37:22 +0800

    Change header title on route navigation.

--- src/renderer/modules/content/header.tsx
+++ src/renderer/modules/content/header.tsx
@@ -1,18 +1,33 @@
+import { useINP } from 'inp-router';
 import { useAppProvider } from '../../providers/app';
 import TransferModal from '../transfer-modal/modal';
 
 const ContentHeader = () => {
   const { user } = useAppProvider();
+  const { current } = useINP();
+
+  const menus: Record<string, { title: string; description: string }> = {
+    dashboard: {
+      title: 'Dashboard',
+      description: `Hello, @${String(user?.auth.actor)}`,
+    },
+    nfts: { title: 'My NFTs', description: 'My NFT Assets' },
+    transactions: {
+      title: 'My Transactions',
+      description: 'View my wallet transactions',
+    },
+    settings: { title: 'App Settings', description: 'Edit settings' },
+  };
 
   return (
     <div className="fixed z-10 w-full border-b border-magnolia bg-gray-50">
       <div className="flex items-center justify-between py-4 px-6">
         <div className="ml-20">
-          <h3 className="font-black text-gray-800 tracking-wide text-lg">
-            DASHBOARD
+          <h3 className="font-black text-gray-800 tracking-wide text-lg uppercase">
+            {menus[current].title}
           </h3>
           <p className="text-neutral-600 text-sm ml-1">
-            Hello, @{String(user?.auth.actor)}
+            {menus[current].description}
           </p>
         </div>
 
